1feafbe5d5005cd03211d74974da583046ed70d2
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2024-10-28 17:42:09 -0300

    feat(house-table): add new house component

---
+++ src/app/(house)/_components/house-table.tsx
@@ -0,0 +1,95 @@
+import { Paginator } from "@/components/ui/pagination";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import type { HouseWithMembers } from "@/models/House";
+
+import Link from "next/link";
+
+type Props = {
+  houses: HouseWithMembers[];
+  page: number;
+};
+
+function HouseTable({ houses = [], page = 1 }: Props) {
+  const maxPageCounter = Math.floor((45 * 10) / 10); // due to the total of items is not returned by the API
+
+  return (
+    <div className="border rounded-lg gap-4 flex flex-col">
+      <Table className="border-b">
+        <TableHeader>
+          <TableRow className="bg-primary/10 hover:bg-primary/20">
+            <TableHead className="w-[200px] font-bold">House</TableHead>
+            <TableHead className="w-[200px] font-bold">Region</TableHead>
+            <TableHead className="font-bold">Sworn Members</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {houses.map((house, index) => (
+            <TableRow
+              key={house.name}
+              className={
+                index % 2 === 0
+                  ? "bg-background hover:bg-accent/50"
+                  : "bg-muted/50 hover:bg-accent/50"
+              }
+            >
+              <TableCell className="font-medium">{house.name}</TableCell>
+              <TableCell>{house.region}</TableCell>
+              <TableCell>
+                <div className="inline-flex gap-1 flex-wrap text-pretty">
+                  {house.swornMembers.length > 0 ? (
+                    house.swornMembers.map((member) => (
+                      <Link key={member.id} href={`/character/${member.id}`}>
+                        {member.isDead ? (
+                          <TooltipProvider>
+                            <Tooltip>
+                              <TooltipTrigger className="hover:underline cursor-pointer">
+                                {member.name}
+                                <sup className="text-red-500 text-xs">*</sup>
+                              </TooltipTrigger>
+                              <TooltipContent>
+                                <p>dead {member.deadAt}</p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        ) : (
+                          <span className="hover:underline cursor-pointer">
+                            {member.name}
+                          </span>
+                        )}
+                      </Link>
+                    ))
+                  ) : (
+                    <i className="text-neutral-400">
+                      This house has no sworn members
+                    </i>
+                  )}
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <small className="text-left text-muted-foreground ml-2">
+        <span className="text-red-500">*</span> characters with dead status
+      </small>
+
+      <Paginator currentPage={page} pageCount={maxPageCounter} />
+    </div>
+  );
+}
+export default HouseTable;
