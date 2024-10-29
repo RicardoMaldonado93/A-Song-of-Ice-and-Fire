7516750b3f34cf7a43034a44eb7ad9d7e8c58bba
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2024-10-29 11:17:04 -0300

    feat(layout): add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export const metadata: Metadata = {
-  title: "A Song of Ice and Fire",
+  title: "An Interface of Ice and Fire",
   description:
     "A challenge generated with NextJS v15 and Tailwind with Shadcn UI",
 };
@@ -16,9 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="dark grid grid-flow-dense gap-2 bg-background !px-4 font-sans antialiased md:gap-4 ">
+      <body className="dark grid grid-cols-1 grid-rows-[auto,1fr,auto] justify-center gap-2 bg-background !px-4 font-sans antialiased md:gap-4 ">
         {modal}
         {children}
+        <footer className="flex min-h-16 items-center justify-center text-balance text-center text-sm text-muted-foreground gap-1">
+          <p className="text-xs text-muted-foreground">
+            Made with 🖤 by Ricardo Maldonado
+          </p>
+
+          <a
+            className="hover:underline"
+            href="https://github.com/RicardoMaldonado93/A-Song-of-Ice-and-Fire"
+          >
+            <GitHubLogoIcon />
+          </a>
+        </footer>
       </body>
     </html>
   );
