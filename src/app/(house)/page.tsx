428a39d845e5c41e468cdd82846838099526c688
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2024-10-29 11:16:43 -0300

    style(house): update style

--- src/app/(house)/page.tsx
+++ src/app/(house)/page.tsx
@@ -10,14 +10,14 @@ export default async function Houses({ searchParams }: PageProps) {
   const housesWithMembers = await getHousesWithSwornMembers(page);
 
   return (
-    <section className="max-w-[1440px] mx-auto">
-      <h2 className="text-2xl font-bold my-4">A Song of Ice and Fire</h2>
+    <div className="max-w-[1440px] mx-auto w-full min-h-full">
+      <h2 className="text-2xl font-bold my-4">An Interface of Ice and Fire</h2>
 
       {housesWithMembers.length > 0 ? (
         <HouseTable houses={housesWithMembers} page={page} />
       ) : (
         <NoRecordsAdvice />
       )}
-    </section>
+    </div>
   );
 }
